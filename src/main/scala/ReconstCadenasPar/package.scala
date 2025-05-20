e9c4decb7d7ac25d443a62a76798605fed078caa
Ckancord33 <niccord33@gmail.com>
2025-05-20 16:59:02 -0500

    primera version de ingenuo paralelo

--- src/main/scala/ReconstCadenasPar/package.scala
+++ src/main/scala/ReconstCadenasPar/package.scala
@@ -1,7 +1,10 @@
-import common._
-import scala.collection.parallel.CollectionConverters._
-import Oraculo._
-import ArbolSufijos._
+import common.*
+
+import scala.collection.parallel.CollectionConverters.*
+import Oraculo.*
+import ArbolSufijos.*
+
+import scala.collection.parallel.ParSeq
 package object ReconstCadenasPar {
   // Ahora versiones paralelas
 
@@ -9,7 +12,36 @@ package object ReconstCadenasPar {
     // recibe la longitud de la secuencia que hay que reconstruir (n), y un oráculo para esa secuencia
     // y devuelve la secuencia reconstruida
     // Usa paralelismo de tareas
-    ???
+    def cadenasDeTamanoN(tamano: Int, combParcial: LazyList[Seq[Char]]): LazyList[Seq[Char]] = {
+      if (tamano == n) combParcial
+      else {
+        val nuevaCombParcial = for {
+          comb <- combParcial
+          letra <- alfabeto
+        } yield comb :+ letra
+        cadenasDeTamanoN(tamano + 1, nuevaCombParcial)
+      }
+    }
+    val mitad = alfabeto.size / 2
+    val (primerMitad, segundaMitad) = alfabeto.splitAt(mitad)
+
+    def explorarConPrefijos(prefijos: Seq[Char]): Option[Seq[Char]] = {
+      val combinaciones = for {
+        letra <- prefijos.to(LazyList)
+      } yield LazyList(Seq(letra))
+
+      combinaciones
+        .flatMap(pref => cadenasDeTamanoN(1, pref))
+        .find(o)
+    }
+
+    val t1 = task {
+      explorarConPrefijos(primerMitad)
+    }
+    val t2 = task {
+      explorarConPrefijos(segundaMitad)
+    }
+    t1.join().getOrElse(t2.join().getOrElse(Seq()))
   }
 
   def reconstruirCadenaMejoradoPar(umbral: Int)(n: Int, o: Oraculo): Seq[Char] = {
